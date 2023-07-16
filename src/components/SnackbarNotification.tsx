65e160b358ae423cc9eb7e86f704d003b17df236
Olti <oltiongashi21@gmail.com>
2023-07-16 17:28:34 +0200

    make snackbar slide from bottom

--- src/components/SnackbarNotification.tsx
+++ src/components/SnackbarNotification.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
-import { Alert, AlertColor, AlertTitle, Snackbar } from '@mui/material';
+import { Alert, AlertColor, AlertTitle, Slide, SlideProps, Snackbar } from '@mui/material';
 import CancelIcon from '@mui/icons-material/Cancel';
 import ErrorIcon from '@mui/icons-material/Error';
 import InfoIcon from '@mui/icons-material/Info';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { theme as twinTheme } from 'twin.macro';
+
+type TransitionProps = Omit<SlideProps, 'direction'>;
+
+function TransitionUp(props: TransitionProps) {
+  return <Slide {...props} direction="up" />;
+}
 
 type SnackbarNotificationProps = {
   id: string | undefined;
@@ -37,6 +44,12 @@ export default function SnackbarNotification({
       key={id}
       open={open}
       onClose={onClose}
+      TransitionComponent={TransitionUp}
+      sx={{
+        '& .MuiAlert-message': {
+          fontSize: twinTheme`fontSize.sm` as string,
+        },
+      }}
     >
       <Alert
         variant="secondary"
