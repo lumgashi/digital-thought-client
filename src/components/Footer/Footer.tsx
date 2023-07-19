ed32ab757d40a983b7749b11e75f5c4768432bb9
Olti <oltiongashi21@gmail.com>
2023-07-19 22:37:53 +0200

    implement footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import { Box, Divider, Typography } from '@mui/material';
+import { theme as twinTheme } from 'twin.macro';
+
+import { useColorMode } from '@/providers/ColorModeProvider';
+
+export default function Footer() {
+  const { colorMode } = useColorMode();
+
+  return (
+    <footer>
+      <Box
+        sx={{
+          backgroundColor:
+            colorMode === 'dark'
+              ? (twinTheme`colors.neutral.300` as string)
+              : (twinTheme`colors.gray.300` as string),
+        }}
+      >
+        <div tw="mx-auto py-4 px-4 sm:px-6 lg:py-16 lg:px-8">
+          <div tw="flex flex-col space-y-4">
+            <div tw="flex flex-row justify-between">
+              <Typography variant="h3">digitalthought.com</Typography>
+
+              <Link href="/">
+                <span>LOGO HERE</span>
+              </Link>
+            </div>
+
+            <Divider
+              tw="h-0.5"
+              sx={{
+                '&.MuiDivider-root': {
+                  backgroundColor:
+                    colorMode === 'dark'
+                      ? (twinTheme`colors.neutral.400` as string)
+                      : (twinTheme`colors.gray.400` as string),
+                },
+              }}
+            />
+
+            <div tw="flex flex-row justify-end">
+              <Typography>© 2023 digitalthought.com</Typography>
+            </div>
+          </div>
+        </div>
+      </Box>
+    </footer>
+  );
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,29 @@
-import Header from '@/components/Header';
 import React from 'react';
+import { Container } from '@mui/material';
+
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import theme from '@/config/theme';
 
 interface LayoutProps {
   children: NonNullable<React.ReactNode>;
+  spacing?: number;
+  maxWidth?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | false;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, spacing = 0, maxWidth = 'lg' }: LayoutProps) {
   return (
-    <div tw="flex flex-col">
+    <div tw="flex flex-col h-screen">
       <Header />
-      {children}
+      <Container
+        disableGutters
+        tw="flex-grow"
+        sx={[spacing > 0 && { padding: theme.spacing(spacing, 0) }]}
+        maxWidth={maxWidth}
+      >
+        {children}
+      </Container>
+      <Footer />
     </div>
   );
 }
